5e965118dd73952f8ae221632acf01e3472a0ee3
stas scorobogatskii <stas.scorobogatskii@student.ehb.be>
2022-10-16 15:55:59 -0700

    1-2.js stas

---
+++ exercises/1-functions/1-2.js
@@ -0,0 +1,41 @@
+"use strict"
+
+drawcoloredbox()
+
+function
+drawcoloredbox(){
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    let context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.moveTo(50,50);
+    context.lineTo(200,200);
+    context.strokeStyle = "red";
+    context.lineWidth = "5";
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(50,200);
+    context.lineTo(200,50);
+    context.strokeStyle = "red";
+    context.lineWidth = "5";
+    context.stroke();
+
+ context.beginPath();
+    context.moveTo(50,50);
+    context.lineTo(50,200);
+    context.lineTo(200,200);
+    context.lineTo(200,50);
+    context.lineTo(50,50);
+    context.strokeStyle = "black";
+    context.lineWidth = "5";
+    context.stroke();
+
+   
+
+
+
+
+}
